649dbe32d7ce9fe7ebace55db127160cebabd175
abderrahman504 <rahmankhsherb@gmail.com>
2023-04-29 21:24:34 +0300

    Added more djikstra tests

--- src/test/java/com/example/DijkstraTest.java
+++ src/test/java/com/example/DijkstraTest.java
@@ -22,7 +22,7 @@ public class DijkstraTest {
 	}
 	
 	@Test
-	public void edge_traversal_test1()
+	public void edge_traversal_test_1()
 	{
 		graph.loadGraphFromFile("TestGraphs/graph1.txt");
 		int[] result = graph.run_dijkstra(0);
@@ -34,9 +34,8 @@ public class DijkstraTest {
 	}
 	
 	@Test
-	public void edge_traversal_test2()
+	public void edge_traversal_test_2()
 	{
-		IGraph graph = new Graph();
 		graph.loadGraphFromFile("TestGraph/graph1.txt");
 		int[] result = graph.run_dijkstra(1);
 		assertEquals(Integer.MAX_VALUE, result[0]);
@@ -48,11 +47,46 @@ public class DijkstraTest {
 	}
 
 	@Test
-	public void shortest_path_test()
+	public void edge_traversal_test_3()
 	{
-		IGraph graph = new Graph();
+		graph.loadGraphFromFile("TestGraph/graph2.txt");
+		int[] result = graph.run_dijkstra(1);
+		assertEquals(Integer.MAX_VALUE, result[0]);
+	}
+
+	@Test
+	public void shortest_path_test_1()
+	{
 		graph.loadGraphFromFile("TestGraph/graph2.txt");
 		int[] result = graph.run_dijkstra(0);
 		assertEquals(4, result[2]);
 	}
+
+	@Test
+	public void shortest_path_test_2()
+	{
+		graph.loadGraphFromFile("TestGraph/graph2.txt");
+		int[] result = graph.run_dijkstra(0);
+		assertEquals(24, result[3]);
+	}
+
+	@Test
+	public void loops_test_1()
+	{
+		graph.loadGraphFromFile("TestGraphs/graph3.txt");
+		int[] result = graph.run_dijkstra(0);
+		//Check path to node 1 and 2 doesn't include loops. Currently can't do it because djikstra doesn't return paths.
+	}
+
+	@Test
+	public void loops_test_2()
+	{
+		graph.loadGraphFromFile("TestGraphs/graph4.txt");
+		int[] result = graph.run_dijkstra(0);
+		assertEquals(Integer.MAX_VALUE, result[4]);
+		assertEquals(Integer.MAX_VALUE, result[5]);
+		assertEquals(Integer.MAX_VALUE, result[6]);
+		//Verify paths to all nodes
+	}
+
 }
