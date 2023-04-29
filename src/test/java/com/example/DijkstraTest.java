c31545e775a97a5032222c61be0e5f2871ddd923
abderrahman504 <rahmankhsherb@gmail.com>
2023-04-29 19:37:30 +0300

    Added Second edge traversal test for djikstra

--- src/test/java/com/example/DijkstraTest.java
+++ src/test/java/com/example/DijkstraTest.java
@@ -26,4 +26,27 @@ public class DijkstraTest {
 		assertEquals(6, result.get(4).intValue());
 		assertEquals(2, result.get(5).intValue());
 	}
+	
+	@Test
+	public void edge_traversal_test2()
+	{
+		IGraph graph = new Graph();
+		graph.loadGraphFromFile("TestGraph/graph1.txt");
+		ArrayList<Integer> result = graph.run_dijkstra(1);
+		assertEquals(Integer.MAX_VALUE, result.get(0).intValue());
+		assertEquals(Integer.MAX_VALUE, result.get(1).intValue());
+		assertEquals(Integer.MAX_VALUE, result.get(2).intValue());
+		assertEquals(Integer.MAX_VALUE, result.get(3).intValue());
+		assertEquals(Integer.MAX_VALUE, result.get(4).intValue());
+		assertEquals(Integer.MAX_VALUE, result.get(5).intValue());
+	}
+
+	@Test
+	public void shortest_path_test()
+	{
+		IGraph graph = new Graph();
+		graph.loadGraphFromFile("TestGraph/graph2.txt");
+		ArrayList<Integer> result = graph.run_dijkstra(0);
+		assertEquals(4, result.get(2).intValue());
+	}
 }
