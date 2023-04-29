a25be99ae11b8660dd0c134065a7b2e8a8a822b6
abderrahman504 <rahmankhsherb@gmail.com>
2023-04-29 19:42:10 +0300

    Changed djikstra tests to fit new return types

--- src/test/java/com/example/DijkstraTest.java
+++ src/test/java/com/example/DijkstraTest.java
@@ -38,13 +38,13 @@ public class DijkstraTest {
 	{
 		IGraph graph = new Graph();
 		graph.loadGraphFromFile("TestGraph/graph1.txt");
-		ArrayList<Integer> result = graph.run_dijkstra(1);
-		assertEquals(Integer.MAX_VALUE, result.get(0).intValue());
-		assertEquals(Integer.MAX_VALUE, result.get(1).intValue());
-		assertEquals(Integer.MAX_VALUE, result.get(2).intValue());
-		assertEquals(Integer.MAX_VALUE, result.get(3).intValue());
-		assertEquals(Integer.MAX_VALUE, result.get(4).intValue());
-		assertEquals(Integer.MAX_VALUE, result.get(5).intValue());
+		int[] result = graph.run_dijkstra(1);
+		assertEquals(Integer.MAX_VALUE, result[0]);
+		assertEquals(Integer.MAX_VALUE, result[1]);
+		assertEquals(Integer.MAX_VALUE, result[2]);
+		assertEquals(Integer.MAX_VALUE, result[3]);
+		assertEquals(Integer.MAX_VALUE, result[4]);
+		assertEquals(Integer.MAX_VALUE, result[5]);
 	}
 
 	@Test
@@ -52,7 +52,7 @@ public class DijkstraTest {
 	{
 		IGraph graph = new Graph();
 		graph.loadGraphFromFile("TestGraph/graph2.txt");
-		ArrayList<Integer> result = graph.run_dijkstra(0);
-		assertEquals(4, result.get(2).intValue());
+		int[] result = graph.run_dijkstra(0);
+		assertEquals(4, result[2]);
 	}
 }
