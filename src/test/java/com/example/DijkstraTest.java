5b750cffe6611169c97b3b2a22afde3e3e926971
abderrahman504 <rahmankhsherb@gmail.com>
2023-04-30 17:14:01 +0300

    Fixing djikstra tests

--- src/test/java/com/example/DijkstraTest.java
+++ src/test/java/com/example/DijkstraTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import com.example.graph.Graph;
@@ -17,11 +19,12 @@ public class DijkstraTest {
 		double[] distances = new double[graph.size()];
 		int[] predecessors = new int[graph.size()];
 		graph.run_dijkstra(0, distances, predecessors);
-		assertEquals(0, Double.valueOf(distances[0]).intValue());
-		assertEquals(4, Double.valueOf(distances[2]).intValue());
-		assertEquals(1, Double.valueOf(distances[3]).intValue());
-		assertEquals(6, Double.valueOf(distances[4]).intValue());
-		assertEquals(2, Double.valueOf(distances[5]).intValue());
+		assertEquals(0, (int)distances[0]);
+		assertEquals(3, (int)distances[1]);
+		assertEquals(4, (int)distances[2]);
+		assertEquals(1, (int)distances[3]);
+		assertEquals(6, (int)distances[4]);
+		assertEquals(2, (int)distances[5]);
 	}
 	
 	@Test
@@ -32,12 +35,12 @@ public class DijkstraTest {
 		double[] distances = new double[graph.size()];
 		int[] predecessors = new int[graph.size()];
 		graph.run_dijkstra(1, distances, predecessors);
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[0]).intValue());
-		assertEquals(0, Double.valueOf(distances[1]).intValue());
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[2]).intValue());
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[3]).intValue());
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[4]).intValue());
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[5]).intValue());
+		assertTrue(Double.isInfinite(distances[0]));
+		assertEquals(0, (int)distances[1]);
+		assertTrue(Double.isInfinite(distances[2]));
+		assertTrue(Double.isInfinite(distances[3]));
+		assertTrue(Double.isInfinite(distances[4]));
+		assertTrue(Double.isInfinite(distances[5]));
 	}
 
 	@Test
@@ -48,7 +51,7 @@ public class DijkstraTest {
 		double[] distances = new double[graph.size()];
 		int[] predecessors = new int[graph.size()];
 		graph.run_dijkstra(1, distances, predecessors);
-		assertEquals(Integer.MAX_VALUE, Double.valueOf(distances[0]).intValue());
+		assertTrue(Double.isInfinite(distances[0]));
 	}
 
 	@Test
